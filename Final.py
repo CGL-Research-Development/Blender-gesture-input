053c7652f1c4c1a9019c4d703d40febbb64385b9
Aeroscythe <coolyogita2896@gmail.com>
2021-07-20 11:41:38 +0530

    Added some more gestures

--- Final.py
+++ Final.py
@@ -12,8 +12,6 @@ mp_hands = mp.solutions.hands
 
 pyautogui.FAILSAFE = False
 
-sourcefile = open('Landmarks.txt', 'w')       #### Printing landmark locations to ext file ####
-
 Flag = 1        #### Execution state ####
 starttime = 0
 endtime = 0
@@ -22,7 +20,7 @@ Main_switch = 0   ###turns code on or off
 ## FPS counter
 last_frame_time, current_frame_time = 0, 0
 
-i = 1       ## For indexing output ##
+index = 1   ### Master index ###
 
 #### Lists store landmarks ####
 xlmarks = []
@@ -91,7 +89,7 @@ with mp_hands.Hands(
             ymouse = np.interp(ylmarks, (0, h), (0, 1080))
 
             if(Flag == 1):
-              ###### 1. controls for main switch ######
+              ###### 0. controls for main switch ######
               if(589 <= xlmarks[8] < xlmarks[12] <= 639 and 0 < ylmarks[8] < 80 and 0 < ylmarks[12] < 80 and Main_switch == 0):
                 Main_switch = 1
                 Flag = 0
@@ -99,6 +97,7 @@ with mp_hands.Hands(
                 ## Pause time ##
                 starttime = int(time.time())
                 endtime = starttime + 60
+              
               elif(589 <= xlmarks[8] < xlmarks[12] <= 639 and 0 < ylmarks[8] < 80 and 0 < ylmarks[12] < 80 and Main_switch == 1):
                 Main_switch = 0
                 Flag = 0
@@ -106,9 +105,9 @@ with mp_hands.Hands(
                 ## Pause time ##
                 starttime = int(time.time())
                 endtime = starttime + 60
-              ##### Main switch end #####
+              ######################################################################################
 
-              ##### 2. Edit mode [Press Tab] #####
+              ##### 1. Edit mode [Press Tab] #####
               if((0 < xlmarks[16] < xlmarks[12] < xlmarks[8] < 90) and (0 < ylmarks[8] < 80) and (0 < ylmarks[12] < 80) and (0 < ylmarks[16] < 80) and Main_switch == 1):
                 Flag = 0
                 
@@ -117,36 +116,64 @@ with mp_hands.Hands(
                 endtime = starttime + 40
 
                 keyboard.press_and_release('Tab')
-              ##### Edit mode end #####
+              ######################################################################################
 
-              ##### 3. Drag ##### [WIP]
-              if(math.dist((xlmarks[4], ylmarks[4]), (xlmarks[8], ylmarks[8])) <= 20 and Main_switch == 1):
+              ##### 2. Drag selection #####
+              if(math.dist((xlmarks[8], ylmarks[8]), (xlmarks[12], ylmarks[12])) <= 7 and Main_switch == 1):
+                index = 2
                 ######### No pause time in drag as we require a continuous input for that one #########
-                mouse.drag(pyautogui.position().x, pyautogui.position().y, xmouse[8], ymouse[8])
-                print(pyautogui.position())
-              
-              # ##### 4. Select stuff #####
-              if(math.dist((xlmarks[8], ylmarks[8]), (xlmarks[12], ylmarks[12])) <= 10 and Main_switch == 1):
+                mouse.drag(pyautogui.position().x, pyautogui.position().y, xmouse[9], ymouse[9])
+              ######################################################################################
+
+              ##### 3. Move mode #####
+              if(math.dist((xlmarks[4], ylmarks[4]), (xlmarks[8], ylmarks[8])) <= 20 and Main_switch == 1):
                 Flag = 0
-                
+                index = 3
+
                 ## Pause time ##
                 starttime = int(time.time())
                 endtime = starttime + 40
 
-                pyautogui.click(button='left')
+                keyboard.press_and_release('g')
+              ######################################################################################
+
+              ##### 4. Rotate mode #####
+              if(math.dist((xlmarks[4], ylmarks[4]), (xlmarks[12], ylmarks[12])) <= 20 and Main_switch == 1):
+                Flag = 0
+                index = 3
+
+                ## Pause time ##
+                starttime = int(time.time())
+                endtime = starttime + 40
+
+                keyboard.press_and_release('r')
+              ######################################################################################
+                            
+              ##### Last-ish: Press esc #####
+              if(Main_switch == 1 and math.dist((xlmarks[4], ylmarks[4]), (xlmarks[20], ylmarks[20])) <= 20):
+                keyboard.press_and_release('esc')
+              ######################################################################################
+
+              ##### Last: Move mouse around #####
+              if(Main_switch == 1):
+                ######### No pause time in move as we require a continuous input for that one #########
+                mouse.move(xmouse[9], ymouse[9])
+                # # # # # # print(pyautogui.position())
+              ######################################################################################
               
         mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-      i += 1
     
     else:
       count = 0
             
     cv2.putText(image, fps, (20, 120), font, 3, (10, 155, 0), 3, cv2.LINE_AA)    ## FPS counter
     cv2.rectangle(image, (0, 0), (90, 80), (255,0,0))
+    
     if(Main_switch == 1):
       cv2.rectangle(image, (589, 0), (639, 80), (0,0,255))
     elif(Main_switch == 0):
       cv2.rectangle(image, (589, 0), (639, 80), (0,255,0))
+    
     cv2.imshow('MediaPipe Hands', image)
     
     # # # # # # # # # # # # # # # endtime = time.time()     ## runtime counter ##
@@ -155,5 +182,4 @@ with mp_hands.Hands(
     if cv2.waitKey(2) & 0xFF == 27:
       break
 
-sourcefile.close()
 cap.release()
